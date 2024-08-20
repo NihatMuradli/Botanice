c59140263f3730c87fd23387fa1fff5971d28c5b
Alibali Alibayov <105583875+AlibaliAlibayov@users.noreply.github.com>
2024-08-20 23:18:15 +0400

    frontend-update-v11
    update of welcome side

--- src/main/resources/static/js/elements/welcome.js
+++ src/main/resources/static/js/elements/welcome.js
@@ -5,10 +5,30 @@ const helloCard = document.querySelector(".hello-card");
 const helloImage = document.querySelector(".hello-img");
 
 let wrapWidth = wrap.offsetWidth;
-textContainer.style.left =  `${wrapWidth * 0.17}px`;
+console.log(wrapWidth);
+textContainer.style.left =  `${wrapWidth * 0.1}px`;
 helloCard.style.left =  `${wrapWidth * 0.8}px`;
-helloImage.style.left =  `${wrapWidth * 0.35}px`;
+helloImage.style.left =  `${wrapWidth * 0.4}px`;
 
-if (wrapWidth < 1680) {
-    titleStroke.setAttribute("y", "110");
+if (wrapWidth <= 1680 && wrapWidth > 1440) {
+    titleStroke.setAttribute("y", "130");
+}else if(wrapWidth <= 1440 && wrapWidth > 1280){
+    titleStroke.setAttribute("y", "102");
+}else if(wrapWidth <= 1280 && wrapWidth > 1024){
+    helloCard.style.left =  `${wrapWidth * 0.75}px`;
+    titleStroke.setAttribute("y", "79");
+    titleStroke.setAttribute("stroke-width","2")
+}else if(wrapWidth <= 1024 && wrapWidth > 768){
+    helloCard.style.left =  `${wrapWidth * 0.75}px`;
+    helloImage.style.left =  `${wrapWidth * 0.45}px`;
+    titleStroke.setAttribute("y", "65");
+    titleStroke.setAttribute("stroke-width","2")
+}else if(wrapWidth <= 768 && wrapWidth > 576){
+    helloCard.style.left =  `${wrapWidth * 0.75}px`;
+    helloImage.style.left =  `${wrapWidth * 0.50}px`;
+    titleStroke.setAttribute("y", "52");
+    titleStroke.setAttribute("stroke-width","1.5")
+}else if( wrapWidth < 576){
+    titleStroke.setAttribute("y", "52");
+    titleStroke.setAttribute("stroke-width","1.5")
 }
