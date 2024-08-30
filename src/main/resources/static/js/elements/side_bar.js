bb68b10da62b7f53eb7f93a71935ede2fb1781ad
AlibaliAlibayov <alibeyovalibali136@gmail.com>
2024-08-30 12:09:32 +0400

    frontend-update-v12.1
    team card first demo

--- src/main/resources/static/js/elements/side_bar.js
+++ src/main/resources/static/js/elements/side_bar.js
@@ -1,8 +1,20 @@
 const sideBar = document.querySelector(".side-bar");
 const sideBarBtn = document.querySelector(".side-btn");
+const body = document.querySelector("body");
 
 sideBarBtn.addEventListener("click", function() {
   sideBar.classList.toggle("active");
   menu.classList.toggle("active");
   checkbox.checked = !checkbox.checked;
 });
+
+function responsiveSideBar() {
+  let bodyWidth = body.offsetWidth;
+
+  if (bodyWidth >= 2000) {
+    sideBar.style.backgroundColor = "#457744b4"; 
+  }
+}
+
+window.addEventListener('resize', responsiveSideBar);
+window.addEventListener('DOMContentLoaded', responsiveSideBar);
