8005ace922d4789a1fd7b4b5316a011b672f1c4e
NihatMuradli <106442654+NihatMuradli@users.noreply.github.com>
2024-09-09 23:45:02 +0400

    Update user.js

--- src/main/resources/static/js/elements/user.js
+++ src/main/resources/static/js/elements/user.js
@@ -1,6 +1,33 @@
-let userLoged = true;
-const loggedHeader = document.querySelector(".header");
+document.addEventListener('DOMContentLoaded', async function () {
+    const loggedHeader = document.querySelector(".header");
+    loggedHeader.classList.add("logged");
 
-if(userLoged){
-    loggedHeader.classList.add("logged"); 
-}
+
+
+    try {
+        const token = localStorage.getItem('jwtToken');
+
+        if (token) {
+            const response = await fetch('http://localhost:5000/api/users/validate', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+
+            if (response.ok) {
+                //User exists
+                alert("+")
+            } else {
+                //Token is not validated so no user
+                alert("-")
+            }
+        } else {
+            // token wasnt found which means no user is logged in
+            alert("-")
+        }
+
+    } catch (error) {
+        console.error('Error during validation:', error);
+    }
+});
