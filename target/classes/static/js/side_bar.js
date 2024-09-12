638415f1371a9ddec8ae4678940100e328216786
NihatMuradli <106442654+NihatMuradli@users.noreply.github.com>
2024-09-13 00:03:00 +0400

    commit

--- target/classes/static/js/side_bar.js
+++
@@ -1,8 +0,0 @@
-const sideBar = document.querySelector(".side-bar");
-const sideBarBtn = document.querySelector(".side-btn");
-
-sideBarBtn.addEventListener("click", function() {
-  sideBar.classList.toggle("active");
-  menu.classList.toggle("active");
-  checkbox.checked = !checkbox.checked;
-});
